230d6ef787dd4334728364cf46f70daef3e77e05
Janne Leppänen <janne@janneleppanen.com>
2020-01-06 18:52:18 +0200

    Make service worker cache assets and routes

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,1 +1,26 @@
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [], {});
+
+workbox.routing.registerRoute(
+  new RegExp("https:.*min.(css|js)"),
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+  new RegExp("https?:.*translation.json"),
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+  new RegExp("https?:.*/graphql/?"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/]
+  }
+);
